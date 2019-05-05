9b85d2359e93518d808d5ed5161760622272f4b5
Erik Oosting <erik99.oosting@gmail.com>
2019-05-05 15:18:11 +0200

    rewrite 2

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
     <div>
         <h1 id="name">Browse files in {{name}}</h1>
         <div id="list">
-            <div v-if="parents.length" class="segment">
-                <a @click="leave()" class="go-up">../</a>
+            <div v-if="parents.length > 0" class="segment">
+                <button @click="leave()" class="go-up">../</button>
             </div>
             <div v-for="file in currentDirectory.children" class="segment">
                 <a v-if="file.type === 'directory'" @click="enter(file)">&#x1F4C2; {{ file.name }}</a>
@@ -14,30 +14,30 @@
 </template>
 
 <script>
-import { Component, Vue } from 'vue-property-decorator';
-
 import FILES from '../assets/files.json';
 
-@Component({
-    components: {}
-})
-export default class App extends Vue {
-    content = FILES;
-    parents = [];
-    currentDirectory = FILES;
+module.exports = {
+    data: {
+        content: FILES,
+        parents: [],
+        currentDirectory: FILES
+    },
 
-    get name() {
-        return [...this.parents.map(x=>x.name), this.currentDirectory.name].join('/');
-    }
+    methods: {
+        enter: function(directory) {
+            this.parents.push(this.currentDirectory);
+            this.currentDirectory = directory;
+        },
+        leave: function () {
+            this.currentDirectory = this.parents.pop();
+        }
 
-    enter(directory) {
-        this.parents.push(this.currentDirectory);
+    },
 
-        this.currentDirectory = directory;
-    }
-
-    leave() {
-        this.currentDirectory = this.parents.pop();
+    computed: {
+        name: {
+            get: function () [...this.parents.map(x=>x.name), this.currentDirectory.name].join('/');
+        }
     }
 }
 </script>
