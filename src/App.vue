26a25eda7c59424df7c8e1a9dec31c9a258e9995
Erik Oosting <erik99.oosting@gmail.com>
2020-06-04 14:12:38 +0200

    added file viewing

--- src/App.vue
+++ src/App.vue
@@ -7,38 +7,49 @@
             </div>
             <div v-for="file in currentDirectory.children" class="segment">
                 <a v-if="file.type === 'directory'" @click="enter(file)">📁 {{ file.name }}</a>
-				<a v-else @click="goto(file)">📄 {{ file.name }}</a>
+                <a v-else @click="goto(file)">📄 {{ file.name }}</a>
             </div>
-        </div>
+	</div>
+	<pre class="fileContent" v-if="fileContent">
+	    <code>
+	    {{fileContent}}
+	    </code>
+	</pre>
     </div>
 </template>
 
 <script>
 import FILES from '../assets/files.json';
 export default {
-	data: function() {
-		return {
-			content: FILES,
-			parents: [],
-			currentDirectory: FILES
-		}
-	},
-	methods: {
-		goto: function(file) {
-			window.location.href = `./files/${file.path}`
-		},
-		enter: function(directory) {
-			this.parents.push(this.currentDirectory);
-			this.currentDirectory = directory;
-		},
-		leave: function() {
-			this.currentDirectory = this.parents.pop();
-		}
-	},
-	computed: {
-		name: function() {
-			return this.currentDirectory.name;
-		}
-	}
+    data: function() {
+        return {
+            content: FILES,
+            parents: [],
+            currentDirectory: FILES,
+            fileContent: null
+        }
+    },
+    methods: {
+        goto: function(file) {
+            const self = this;
+            fetch(`./files/${file.path}`)
+                .then(res => res.text())
+                .then(res => self.fileContent = '\n' + res)
+                .then(()  => this.enter(file))
+        },
+        enter: function(directory) {
+            this.parents.push(this.currentDirectory);
+            this.currentDirectory = directory;
+        },
+        leave: function() {
+            this.currentDirectory = this.parents.pop();
+            this.fileContent = null;
+        }
+    },
+    computed: {
+        name: function() {
+            return this.currentDirectory.name;
+        }
+    }
 }
 </script>
